ff6276aeb2c9c504975c2af89b12b8bd6a69dafd
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-21 20:22:21 +0100

    geo

--- views/userpage.php
+++ views/userpage.php
@@ -1,8 +1,14 @@
-<main class="user-page">
-    <nav>
-        Index
-    </nav>
-    <section>
-        <?= $form_sign_up->output(); ?>
-    </section>
-</main>
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+    <head>
+        <meta charset="utf-8">
+        <title>geoloc</title>
+    </head>
+    <body>
+        <div class="map" style="height:50vh">
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7G7LZUdRZ2oxw1OBWzsHK1yCHKXIXKMs&language=en&region=NL">
+            </script>
+        </div>
+
+    </body>
+</html>
