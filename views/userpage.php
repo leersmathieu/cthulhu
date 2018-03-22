2af2b27af2ca9328af8bd8ed6c1b6dd89091dde2
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-22 04:39:19 +0100

    screen 9

--- views/userpage.php
+++ views/userpage.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html lang="en" dir="rtl">
-    <head>
-        <meta charset="utf-8">
-        <title>geoloc</title>
-    </head>
-    <body>
-        <div class="map" style="height:50vh">
-            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7G7LZUdRZ2oxw1OBWzsHK1yCHKXIXKMs&language=en&region=NL">
-            </script>
-        </div>
 
-    </body>
-</html>
+<div class="map">
+    <img style="height:50vh" src="../assets/img/maps.png" alt="map">
+</div>
+<div class="geoForm">
+    <p>Saisissez votre adresse:</p><input type="text" name="userAdress" value="">
+    <p>Saisissez votre périmètre de recherhe: </p>
+    <select class="perimeter" name="">
+        <option value="none" selected>none</option>
+        <option value="10km">10km</option>
+        <option value="20km">20km</option>
+        <option value="30km">30km</option>
+        <option value="40km">40km</option>
+        <option value="50km">50km</option>
+    </select>
+    <button type="submit" name="setGeo">Mettre à jour</button>
+</div>
