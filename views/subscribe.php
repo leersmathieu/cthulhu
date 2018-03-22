b390919224aedb9dd328ee6da54aa0a1e19c3cd2
leersmathieu <leers.mathieu@gmail.com>
2018-03-22 03:42:32 +0100

    new P

---
+++ views/subscribe.php
@@ -0,0 +1,14 @@
+<main class="subscribe">
+    <nav>
+        Index
+    </nav>
+    <section>
+        <h2>Inscription</h2>
+        <p>Renseigner les champs suivants</p>
+        <input type="text" placeholder="nom">
+        <input type="text" placeholder="prenom"><br/>
+        <input type="mail" placeholder="adresse mail"><br />
+        <input type="password" placeholder="mot de passe">
+
+    </section>
+</main>
